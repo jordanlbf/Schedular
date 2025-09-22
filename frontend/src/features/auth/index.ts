2eb16dfe18ed6b251139a8ba51afae489078a6c6
Jordan Brewis Foulger <jordan.lb.foulger@gmail.com>
2025-09-22 21:15:53 +1000

    chore: delete unused auth feature (not wired into app)

--- frontend/src/features/auth/index.ts
+++
@@ -1,8 +0,0 @@
-// Auth feature exports
-export * from './types';
-export * from './hooks/useAuth';
-export * from './services/auth.api';
-export * from './services/auth.storage';
-
-// Re-export components when they are created
-// export * from './components';

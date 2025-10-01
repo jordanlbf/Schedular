241db5cc12e047b528197eb0ebc954e0d9e28719
Jordan Brewis Foulger <jordan.lb.foulger@gmail.com>
2025-10-01 10:21:38 +1000

    refactor(config): consolidate and modernize configuration files

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,21 +3,33 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  // Global ignores
+  {
+    ignores: ['dist/**', 'build/**', 'node_modules/**', 'coverage/**', '*.config.cjs']
+  },
+  // Main config
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
     ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
     },
-  },
-])
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
+)
